ee61852327139940decc14ec7dbc43029884dd5f
athomas227 <andrewthomas@pursuit.org>
2024-04-20 08:17:44 -0400

    used browser geolocation to add functionality to "track my location" button

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -3,6 +3,24 @@ import { Container, Row, Col } from "react-bootstrap";
 import Map from "../Map/Map";
 
 export default function Home() {
+    const [userLocation, setUserLocation] = useState();
+
+    const handleTrackLocation = () => {
+        if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+            const { latitude, longitude } = position.coords;
+            setUserLocation({ latitude, longitude });
+            },
+            (error) => {
+            console.error('Error getting location:', error);
+            }
+        );
+        } else {
+        console.error('Geolocation is not supported by this browser.');
+        }
+    };
+
     return (
         <div>
             <Container className="mt-4">
@@ -12,11 +30,11 @@ export default function Home() {
                     <p>
                     Use the map to locate the nearest Mr. Softee ice cream truck in your area. The trucks' locations are updated in real-time, so you can always find the freshest and most delicious ice cream treats!
                     </p>
-                    <button className="btn btn-primary" >Track My Location</button>
+                    <button className="btn btn-primary" onClick={handleTrackLocation}>Track My Location</button>
                 </Col>
 
                 <Col md={8}>
-                    <Map />
+                    <Map userLocation={userLocation}/>
                 </Col>
                 </Row>
             </Container>
